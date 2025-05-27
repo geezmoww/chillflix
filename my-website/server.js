b5d736138c4b8e6a97ebff727a6026b00f01cedc
geezmoww <eusaffmorph@gmail.com>
2025-05-27 02:28:37 -0700

    Create server.js

---
+++ my-website/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public')); // serve frontend files
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // broadcast to all clients
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
